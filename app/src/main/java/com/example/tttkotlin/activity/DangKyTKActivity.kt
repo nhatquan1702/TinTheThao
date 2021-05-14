ed048d13c0dec93a621a511dada5cc494c1cba80
nhatm <nhatquan1702>
2021-05-14 12:16:56 +0700

    check patten email and pass

--- app/src/main/java/com/example/tttkotlin/activity/DangKyTKActivity.kt
+++ app/src/main/java/com/example/tttkotlin/activity/DangKyTKActivity.kt
@@ -1,30 +1,28 @@
 package com.example.tttkotlin.activity
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.method.HideReturnsTransformationMethod
 import android.text.method.PasswordTransformationMethod
-import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import android.widget.Toast
-import androidx.core.view.doOnLayout
+import androidx.appcompat.app.AppCompatActivity
 import com.example.tttkotlin.R
 import com.example.tttkotlin.api.RetrofitInstance
 import com.example.tttkotlin.model.Status
 import kotlinx.android.synthetic.main.activity_dang_ky_t_k.*
-import kotlinx.android.synthetic.main.activity_dang_ky_t_k.checkShowPassDK
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.util.regex.Pattern
 
 class DangKyTKActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dang_ky_t_k)
-        val animation = AnimationUtils.loadAnimation(this,R.anim.uptodowndiagonal)
+        val animation = AnimationUtils.loadAnimation(this, R.anim.uptodowndiagonal)
         rLayoutDK.setAnimation(animation)
-        val animationx = AnimationUtils.loadAnimation(this,R.anim.tranlation_x)
+        val animationx = AnimationUtils.loadAnimation(this, R.anim.tranlation_x)
         tvNhapEmail.setAnimation(animationx)
         edtNhapEmailDK.setAnimation(animationx)
         tvNhapUserName.setAnimation(animationx)
@@ -59,8 +57,18 @@ class DangKyTKActivity : AppCompatActivity(){
                 edtNhapEmailDK.requestFocus()
                 return@setOnClickListener
             }
+            if(!checkFormatEmail(email)){
+                edtNhapEmailDK.error = "Email không hợp lệ! Ví dụ hợp lệ: Example@gmail.com"
+                edtNhapEmailDK.requestFocus()
+                return@setOnClickListener
+            }
             if(pass1.isEmpty()){
                 edtNhapPass1.error = "Vui lòng kiểm tra lại mật khẩu!"
+                edtNhapPass1.requestFocus()
+                return@setOnClickListener
+            }
+            if(!checkFormatPass(pass1)){
+                edtNhapPass1.error = "Mật khẩu không hợp lệ!"
                 edtNhapPass1.requestFocus()
                 return@setOnClickListener
             }
@@ -75,34 +83,61 @@ class DangKyTKActivity : AppCompatActivity(){
         }
 
     }
-    fun dangKyTaiKhoan(username:String, email:String, pass:String){
+    fun dangKyTaiKhoan(username: String, email: String, pass: String){
         RetrofitInstance.instance.dangKyTaiKhoan(username, email, pass).enqueue(object : Callback<Status> {
             override fun onResponse(call: Call<Status>, response: Response<Status>) {
-                response.body()?.let{
-                    if(it.getStatus()==0){
-                        Toast.makeText(applicationContext,"Đăng ký tài khoản thành công!", Toast.LENGTH_SHORT).show()
+                response.body()?.let {
+                    if (it.getStatus() == 0) {
+                        Toast.makeText(applicationContext, "Đăng ký tài khoản thành công!", Toast.LENGTH_SHORT).show()
                         val intent = Intent(applicationContext, LoginActivity::class.java)
                         startActivity(intent)
                         onBackPressed()
                     }
-                    if(it.getStatus()==1){
-                        edtNhapUsernameDK.error="Vui lòng kiểm tra lại tên người dùng!"
+                    if (it.getStatus() == 1) {
+                        edtNhapUsernameDK.error = "Vui lòng kiểm tra lại tên người dùng!"
                     }
-                    if(it.getStatus()==2){
-                        edtNhapUsernameDK.error="Vui lòng kiểm tra lại email!"
+                    if (it.getStatus() == 2) {
+                        edtNhapUsernameDK.error = "Vui lòng kiểm tra lại email!"
                     }
-                    if(it.getStatus()==3){
-                        edtNhapPass1.error="Vui lòng kiểm tra lại mật khẩu!"
-                        edtNhapPass2.error="Vui lòng kiểm tra lại mật khẩu!"
+                    if (it.getStatus() == 3) {
+                        edtNhapPass1.error = "Vui lòng kiểm tra lại mật khẩu!"
+                        edtNhapPass2.error = "Vui lòng kiểm tra lại mật khẩu!"
                     }
-                    if(it.getStatus()==4){
-                        edtNhapEmailDK.error="Email đã tồn tại!"
+                    if (it.getStatus() == 4) {
+                        edtNhapEmailDK.error = "Email đã tồn tại!"
                     }
                 }
             }
+
             override fun onFailure(call: Call<Status>, t: Throwable) {
-                Toast.makeText(applicationContext,"Lỗi!"+t.message, Toast.LENGTH_SHORT).show()
+                Toast.makeText(applicationContext, "Lỗi!" + t.message, Toast.LENGTH_SHORT).show()
             }
         })
+    }
+    fun checkFormatEmail(email: String): Boolean {
+        val EMAIL_ADDRESS_PATTERN = Pattern.compile(
+                "[a-zA-Z0-9\\+\\.\\_\\%\\-\\+]{1,256}" +
+                        "\\@" +
+                        "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,64}" +
+                        "(" +
+                        "\\." +
+                        "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,25}" +
+                        ")+"
+        )
+        return EMAIL_ADDRESS_PATTERN.matcher(email).matches()
+//        val expression = "[a-zA-Z0-9._-]+@[a-z]+\\\\.+[a-z]+"
+//        val pattern: Pattern = Pattern.compile(expression, Pattern.CASE_INSENSITIVE)
+//        val matcher: Matcher = pattern.matcher(email)
+//        Toast.makeText(applicationContext, matcher.matches().toString(), Toast.LENGTH_SHORT).show()
+//        return matcher.matches()
+    }
+    fun checkFormatPass(pass: String): Boolean {
+        val PASS_PATTERN = Pattern.compile(
+                "^(?=.*[0-9])"
+                        + "(?=.*[a-z])(?=.*[A-Z])"
+                        + "(?=.*[@#$%^&+=])"
+                        + "(?=\\S+$).{8,20}$"
+        )
+        return PASS_PATTERN.matcher(pass).matches()
     }
 }
